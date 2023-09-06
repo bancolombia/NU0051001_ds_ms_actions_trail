29acf2421936da5db7ff764b32253fb28c57d3e6
Jorge Alejandro Aguirre Gutierrez <joaaguir@bancolombia.com.co>
2023-09-06 10:02:02 -0500

    feature

--- deployment/acceptance-test/src/test/resources/karate/generate-random-code.js
+++
@@ -1,9 +0,0 @@
-function fn(length) {
-    var charactersAndNumbers = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    var charactersLength = charactersAndNumbers.length;
-    var result = '';
-    for ( var i = 0; i < length; i++ ) {
-        result += charactersAndNumbers.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-}
